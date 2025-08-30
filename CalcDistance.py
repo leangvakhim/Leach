9369c83a172d951ea1bf50e114e182ff405c2921
leangvakhim <Vakhiml60@gmail.com>
2025-08-30 15:15:33 +0700

    init

--- CalcDistance.py
+++ CalcDistance.py
@@ -1,5 +1,12 @@
-def CalcDistance(Node1, Node2):
-    X = (Node1[0] - Node2[0]) ** 2
-    Y = (Node1[1] - Node2[1]) ** 2
-    Out = (X + Y) ** 0.5
-    return Out
+# def CalcDistance(Node1, Node2):
+#     X = (Node1[0] - Node2[0]) ** 2
+#     Y = (Node1[1] - Node2[1]) ** 2
+#     Out = (X + Y) ** 0.5
+#     return Out
+def CalcDistance(coords1, coords2):
+    # coords1 will be (x1, y1) and coords2 will be (x2, y2)
+    X_dist = (coords1[0] - coords2[0]) ** 2
+    Y_dist = (coords1[1] - coords2[1]) ** 2
+    
+    distance = (X_dist + Y_dist) ** 0.5
+    return distance
