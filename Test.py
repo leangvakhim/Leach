884077693b4311a504887cd677c383d02f1df75d
leangvakhim <Vakhiml60@gmail.com>
2025-08-30 13:00:48 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -14,3 +14,10 @@ y = np.random.uniform(1, 100, size=(100, 1))
 kmeans_model = KMeans(n_clusters=5, random_state=42)
 A = kmeans_model.fit_predict(np.concatenate((x, y), axis=1))
 
+plt.plot(x[A == 0], y[A == 0], marker='s', color=(0.7, 0, 0))
+plt.plot(x[A == 1], y[A == 1], marker='s', color=(1, 0, 0))
+plt.plot(x[A == 2], y[A == 2], marker='s', color=(0, 1, 0))
+plt.plot(x[A == 3], y[A == 3], marker='s', color=(0, 0, 1))
+plt.plot(x[A == 4], y[A == 4], marker='s', color=(0.5, 0, 0.5))
+
+plt.show()
